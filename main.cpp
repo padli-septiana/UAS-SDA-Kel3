e311004f23787ceaa7798b9dda0c827016256852
wanrabbae <wancode.msg@gmail.com>
2024-06-05 11:10:02 +0700

    init list data structure for books data

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include "modul/books/list_buku.h"
 
 using namespace std;
 
 int main()
 {
-    // Your code here
-
+    // example of using show_buku function
+    show_buku(list_buku, 10, 0);
     return 0;
 }
